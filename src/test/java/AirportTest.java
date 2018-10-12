3201edd9fc498bcbc74d56f663cac7162b39c180
Filip Kuszenin <filipkuszenin@gmail.com>
2018-10-12 13:59:38 +0100

    airport.removePlaneFromHanger adding more tests

--- src/test/java/AirportTest.java
+++ src/test/java/AirportTest.java
@@ -7,6 +7,7 @@ public class AirportTest {
 
     Airport airport;
     Plane plane1;
+    Plane plane2;
     Flight flight;
     Passenger passenger;
 
@@ -14,6 +15,7 @@ public class AirportTest {
     public void before() {
         passenger = new Passenger();
         plane1 = new Plane(1, PlaneType.BOEING747, AirlineName.RYANAIR);
+        plane2 = new Plane(2, PlaneType.BOEING747, AirlineName.RYANAIR);
         flight = new Flight("MUN001", "Munich");
         airport = new Airport("EDI");
     }
@@ -39,6 +41,14 @@ public class AirportTest {
         airport.addPlaneToHanger(plane1);
         airport.removePlaneFromHanger(plane1);
         assertEquals(0, airport.countPlanesInHanger());
+    }
+    @Test
+    public void removePlaneFromHangerTwoPlanesInHanger() {
+        airport.addPlaneToHanger(plane1);
+        airport.addPlaneToHanger(plane1);
+        airport.removePlaneFromHanger(plane1);
+        assertEquals(1, airport.countPlanesInHanger());
+        assertEquals(plane1, airport.removePlaneFromHanger(plane1));
     }
 
 
