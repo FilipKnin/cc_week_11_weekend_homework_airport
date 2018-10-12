ce5354652be46fbfdeea52e1172a52478ea46bc5
Filip Kuszenin <filipkuszenin@gmail.com>
2018-10-12 13:25:48 +0100

    Airport.getAirportCode created and tested

--- src/main/java/Airport.java
+++ src/main/java/Airport.java
@@ -1,2 +1,16 @@
+import java.util.ArrayList;
+
 public class Airport {
+
+    private String code;
+    private ArrayList<Plane> hanger;
+
+    public Airport(String code) {
+        this.code = code;
+        this.hanger = new ArrayList<>();
+    }
+
+    public String getAirportCode() {
+        return this.code;
+    }
 }
--- src/test/java/AirportTest.java
+++ src/test/java/AirportTest.java
@@ -1,2 +1,30 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class AirportTest {
+
+    Airport airport;
+    Plane plane1;
+    Flight flight;
+    Passenger passenger;
+
+    @Before
+    public void before() {
+        passenger = new Passenger();
+        plane1 = new Plane(1, PlaneType.BOEING747, AirlineName.RYANAIR);
+        flight = new Flight("MUN001", "Munich");
+        airport = new Airport("EDI");
+    }
+
+    @Test
+    public void hasCode() {
+        assertEquals("EDI", airport.getAirportCode());
+    }
+
+
+
+
+
 }
