42c76fa0f81cfc43903fc2ae4480007b31976dbf
Filip Kuszenin <filipkuszenin@gmail.com>
2018-10-12 11:28:13 +0100

    ENUM PlaneType created. PlaneType.getCapacity created

---
+++ src/main/java/PlaneType.java
@@ -0,0 +1,17 @@
+public enum PlaneType {
+    BOEING747(5),
+    BOEING787(100),
+    AIRBUS330(50),
+    AIRBUS350(80),
+    AIRBUS380(10);
+
+    private final int capacity;
+
+    PlaneType(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+}
--- src/test/java/PlaneTest.java
+++ src/test/java/PlaneTest.java
@@ -1,2 +1,14 @@
+import org.junit.Before;
+
 public class PlaneTest {
+
+    Plane plane;
+    Passenger passenger;
+
+
+    @Before
+    public void before() {
+        plane = new Plane(1, PlaneType.BOEING747, PlaneAirline.RYANAIR);
+        passenger = new Passenger();
+    }
 }
